127717ed3a2fc173639d7cd9ae1d0c78b027ed50
源?띾쾾 <fkdldj48@naver.com>
2024-10-24 13:32:19 +0900

    Feat: 기능 추가 및 기존 코드 보완
    포인트 적립할 수 있는 기능을 추가 했습니다.
    기존 결제 방식 선택과 영수증 출력 선택시 잘못된 선택을 할 경우
    재선택을 하지 못하는 오류를 개선했습니다.

--- src/payment/paymentMain.java
+++ src/payment/paymentMain.java
@@ -6,28 +6,36 @@ public class paymentMain {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("결제 방법을 선택해 주세요 !");
-        System.out.println("1. 현금");
-        System.out.println("2. 카드");
-        System.out.println("3. 카카오 페이");
+        // 결제 금액 입력받기
+        System.out.println("결제 금액을 입력하세요:");
+        double paymentAmount = sc.nextDouble(); // 일단 테스트를 위해 입력값으로 설정
 
-        int choice = sc.nextInt();
         PaymentMethod paymentMethod = null;
+        int choice;
 
-        switch (choice) {
-            case 1:
-                paymentMethod = new paymentCash();
-                break;
-            case 2:
-                paymentMethod = new paymentCard();
-                break;
-            case 3:
-                paymentMethod = new paymentKakao();
-                break;
-            default:
-                System.out.println("잘못된 선택입니다. 다시 선택하세요.");
-                return;
-        }
+        do {
+            System.out.println("결제 방법을 선택해 주세요 !");
+            System.out.println("1. 카드");
+            System.out.println("2. 삼성페이");
+            System.out.println("3. 카카오페이");
+
+            choice = sc.nextInt();
+
+            switch (choice) {
+                case 1:
+                    paymentMethod = new paymentCard(paymentAmount);  // 결제 금액 전달
+                    break;
+                case 2:
+                    paymentMethod = new paymentSamsung(paymentAmount);
+                    break;
+                case 3:
+                    paymentMethod = new paymentKakao(paymentAmount);
+                    break;
+                default:
+                    System.out.println("잘못된 선택입니다. 다시 선택하세요.");
+            }
+        } while (paymentMethod == null); // 유효한 선택이 될 때까지 반복
+
         paymentMethod.processPayment();
 
         sc.close();
